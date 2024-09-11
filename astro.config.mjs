0b8814111de1208f6b76f149441187f5a93716fe
taoguangc <taoguangc@gmail.com>
2024-09-11 19:08:40 +0800

    update astro to 4.15.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://example.com',
+  integrations: [react(), sitemap(), mdx(), markdoc(), keystatic()],
+  output: 'hybrid'
+})
---
+++ keystatic.config.ts
@@ -0,0 +1,54 @@
+import { config, collection, singleton, fields } from '@keystatic/core'
+
+export default config({
+  storage: {
+    kind: 'local'
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*/',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({
+          name: { label: 'Title' },
+          slug: {
+            label: 'SEO-friendly slug',
+            description: 'This will define the file/folder name for this entry'
+          }
+        }),
+        description: fields.text({
+          label: '描述',
+          multiline: true
+        }),
+        pubDate: fields.date({
+          label: '发布日期',
+          description: '发布的日期'
+        }),
+        updatedDate: fields.date({
+          label: '更新日期',
+          description: '更新的日期'
+        }),
+        heroImage: fields.image({
+          label: '大图',
+          directory: './'
+        }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: true
+        })
+      }
+    })
+  },
+  singletons: {
+    settings: singleton({
+      label: 'Settings',
+      path: 'custom/content/path/settings',
+      schema: {}
+    })
+  }
+})
